3b2504422697c8aef6cd522c76365a015807e4e8
praewwich <pwichainao@gmail.com>
2019-03-13 09:42:01 +0700

    disable static storage

--- FoodWeb/FoodWeb/settings.py
+++ FoodWeb/FoodWeb/settings.py
@@ -143,7 +143,7 @@ STATICFILES_DIRS = [
 os.path.join(BASE_DIR, 'FoodApp/static'),
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 MEDIA_URL = '/media/'
