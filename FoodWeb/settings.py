0d20f156954b99f1df2a6421e9fc327a24dc0900
praewwich <pwichainao@gmail.com>
2019-03-13 11:59:10 +0700

    add debug

--- FoodWeb/settings.py
+++ FoodWeb/settings.py
@@ -207,3 +207,18 @@ JET_THEMES = [
 ]
 
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
